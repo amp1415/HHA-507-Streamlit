55a05ad83fd55f168f6ef496281d10d899af007f
Amanda Power <amanda.power@stonybrook.edu>
2021-12-16 18:57:45 -0500

    Update HHA 507.py

--- HHA 507.py
+++ HHA 507.py
@@ -5,9 +5,9 @@ import time
 
 ##LOAD IN CSV 
 @st.cache
-def load_hosptial():
-    hospitaldf = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
-    return hosptialdf
+def load_hospital():
+    hospital_df= pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
+    return hospital_df
 @st.cache 
 def load_inpatient():
     inpatientdf =pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv')
@@ -31,6 +31,19 @@ st.write('3. Where are most of the hospitals located in New York state?')
 st.write('4. How had better care timliness? NY or CA')
 
 #LOAD DATA
-hospitaldf = load_hosptial()
+hospital_df = load_hospital()
 inpatientdf= load_inpatient()
 outpaitentdf= load_outpatient()
+
+##PREVIEW DATA SET
+st.header('Hospital Data Preview')
+st.markdown('This dataset consists of 5,314 hospitals in the United States. Each row represents a unique hospital, referenced by "provider_id", and additional info for each location.')
+st.dataframe(load_hospital())
+
+st.header('Inpatient Data Preview')
+st.markdown('This dataset consists of drg codes for multiple inpatient locations. Each row represents a unique drg code, referenced by "drg_definition", along with payment details for each code.')
+st.dataframe(load_inpatient())
+
+st.header('Outpatient Data Preview')
+st.markdown('This dataset consists of apc codes for multiple outpatient locations. Each row represents a unique apc code, referenced by "apc", along with service and payment details for each code.')
+st.dataframe(load_outpatient())
